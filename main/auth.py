0c605595458857f8ae82fa2a10a78f816f194e7b
Serg Baburin <serg.baburin@gmail.com>
2013-10-27 01:09:12 +0400

    Fixed mistyped.

--- main/auth.py
+++ main/auth.py
@@ -420,7 +420,7 @@ def signin_dropbox():
       callback=flask.url_for(
           'dropbox_authorized',
           _external=True,
-        ).repalce('http', 'https')
+        ).replace('http', 'https')
     )
 
 
